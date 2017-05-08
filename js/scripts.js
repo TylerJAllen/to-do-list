c230f3c25b2a5da4f6980ab76919a87daaa1527a
Jared Eiseman and Tyler Allen <tyler@>
2017-05-08 14:11:54 -0700

    DO MOAR STYLING

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,6 @@ function Task(task,date){
   this.completed = false;
 }
 
-
-
 //user interface logic
 $(function() {
   $("#input-form").submit(function(e){
@@ -16,7 +14,8 @@ $(function() {
     var newTask = new Task(task, date);
 
     $('#task-input').before('<div class="row task"><div class="col-xs-1 left-col"><input type="checkbox" class="checkbox"></div><div class="col-xs-9"><h4>' + newTask.task + '</h4></div><div class="col-xs-2"><h4>' + newTask.date + '</h4></div></div>');
-
+    // $("#task").val("");
+    // $("#date").val("");
   });
   $("#remove-task").click(function(){
     $("input[type='checkbox']:checked").each(function(index){
